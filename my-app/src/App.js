b9f3cfd35001c67a138917faf7b80fb0a3d2ca58
Sushma <sushmareddyvatte@gmail.com>
2024-12-21 10:49:38 +0530

    adding 3 files together

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import Apple from './Apple';
+import Orange from './Orange';
+function App(){
+  return (
+    <div className="App">
+    <Apple name="Ashritha"/>
+    <Orange />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Orange.jsx
@@ -0,0 +1,8 @@
+function Orange()
+{
+    return(
+        <h2>I Love You</h2>
+    )
+}
+export default Orange;
+
